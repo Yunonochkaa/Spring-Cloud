4608c8703efdc538e055e7a994e2568bf2e52436
Yunona <guzel.ru.2015@mail.ru>
2025-03-30 11:25:44 +0500

    upd

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,12 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableConfigServer
-public class DemoApplication {
 
-	public static void main(String[] args) {
+ class DemoApplication {
+		public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
